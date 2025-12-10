5a601f8d459fb95f31fd485ce47a9852bbf71c0a
Shubham <shubhamp1251@gmail.com>
2025-12-10 17:14:49 +0530

    06: E2B Sanboxes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,17 @@
 import { inngest } from "./client";
 import { gemini, createAgent } from "@inngest/agent-kit";
+import {Sandbox} from "@e2b/code-interpreter";
+import { getSandbox } from "./utlis";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
+
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+        const sandbox = await Sandbox.create("buildlyio-nextjs-test");
+        return sandbox.sandboxId;
+    });
     const codeAgent = createAgent({
       name: "code-agent",
       system: "You are an expert next.js developer.  You write readable maintainable code. You write simple Next.js & React snippets.",
@@ -15,8 +22,14 @@ export const helloWorld = inngest.createFunction(
       `Write the following snippet: ${event.data.value}`
     );
 
+    const sandboxUrl = await step.run ("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+
     console.log(output);
 
-    return { success: 'ok', output };
+    return { success: 'ok', output, sandboxUrl };
   }
 );
