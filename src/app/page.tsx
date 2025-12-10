e7da4c5d57b947d10c22d9cedafc1df952e95de0
Shubham <shubhamp1251@gmail.com>
2025-12-10 15:24:41 +0530

    05: ai jobs setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,25 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 
 export default function Home() {
+  const [value, setValue] = useState("");
   const trpc = useTRPC();
   const invoke = useMutation(trpc.invoke.mutationOptions({}));
   return (
     <div className="p-4 max-w-7xl mx-auto">
+      <Input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
       <Button
         onClick={() =>
           invoke.mutate({
-            text: "Test2",
+            value: value,
           })
         }
       >
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,22 @@
 import { inngest } from "./client";
+import { gemini, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert next.js developer.  You write readable maintainable code. You write simple Next.js & React snippets.",
+      model: gemini({ model: "gemini-flash-latest" }),
+    });
+
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`
+    );
+
+    console.log(output);
+
+    return { success: 'ok', output };
+  }
 );
